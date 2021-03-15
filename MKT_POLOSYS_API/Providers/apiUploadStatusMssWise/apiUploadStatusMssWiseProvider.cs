fddc6807e11363dfe4724b1aa1832350704bea87
juansarea <juansarea@gmail.com>
2021-03-15 09:29:07 +0700

    Penyesuaian Path Controller

---
+++ MKT_POLOSYS_API/Providers/apiUploadStatusMssWise/apiUploadStatusMssWiseProvider.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MKT_POLOSYS_API.DataAccess;
+using MKT_POLOSYS_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MKT_POLOSYS_API.Providers.apiUploadStatusWise
+{
+    public class apiUploadStatusMssWiseProvider
+    {
+        private WISE_STAGINGContext context = new WISE_STAGINGContext();
+        public List<ProcessResultModel> procUpdStatWIse(string parameterBody)
+        {
+            var connectionString = context.Database.GetDbConnection().ConnectionString;
+            List<ProcessResultModel> procUpdStatWIse = new List<ProcessResultModel>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //Declare COnnection                
+                var querySstring = "spMKT_POLO_APIUPDATEDATA_TO_MSSWISE";
+                SqlCommand command = new SqlCommand(querySstring, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                //Define Query Parameter
+                command.Parameters.AddWithValue("@parameterBody", parameterBody);
+
+                //open Connection
+                command.Connection.Open();
+
+                //PRoses Sp
+                SqlDataReader rd = command.ExecuteReader();
+                while (rd.Read())
+                {
+                    ProcessResultModel data = new ProcessResultModel();
+                    data.message = rd[0].ToString();
+                    data.codeMessage = rd[1].ToString();
+                    data.errorMessage = rd[2].ToString();
+                    procUpdStatWIse.Add(data);
+                }
+
+                //Connection Close
+                command.Connection.Close();
+
+            }
+
+            return procUpdStatWIse;
+
+        }
+    }
+}
