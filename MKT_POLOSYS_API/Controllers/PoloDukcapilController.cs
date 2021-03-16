6c0ca75a853d65db4cbcd1344c05ad8661d7389a
Ryan Maulana Putra <ryputra@wom.co.id>
2021-03-17 01:32:23 +0700

    Polo Dukcapil API done controller: - change API URI to use controller name - remove test function - remove unnecessary several comment line provider: - added function update dukcapil API response

--- MKT_POLOSYS_API/Controllers/PoloDukcapilController.cs
+++ MKT_POLOSYS_API/Controllers/PoloDukcapilController.cs
@@ -14,39 +14,14 @@ namespace MKT_POLOSYS_API.Controllers
 {
     public class PoloDukcapilController : ControllerBase
     {
-        [HttpPost("api/CheckDukcapil")]
+        [HttpPost("api/[controller]")]
         [Produces("application/json")]
 
         public async Task<IActionResult> CheckDukcapil([FromBody]IDictionary<string, string> body)
         {
-            string parameterBody = "";
-            //using (StreamReader stream = new StreamReader(Request.Body))
-            //{
-            //    parameterBody = await stream.ReadToEndAsync();
-            //}
-
-            //Dictionary<string, string> test = new Dictionary<string, string>();
-
-            //test.Add("test", "hi");
             await PoloDukcapilProvider.getDukcapilQueue(body["dataSource"], body["queueUID"]);
 
-            return Ok(body);
-
-            //MMktPoloQuestionLabelProvider mMktPoloQuestionLabelProvider = new MMktPoloQuestionLabelProvider();
-            //var data = mMktPoloQuestionLabelProvider.getQuestionLabel(parameterBody);
-
-            //if (data.Count == 0)
-            //{
-            //    return NotFound(data);
-            //}
-            //return Ok(data);
-        }
-
-        [HttpPost("api/test")]
-        [Produces("application/json")]
-        public async Task<IActionResult> test([FromBody]IDictionary<string, string> body)
-        {
-            return Ok("delete me please");
+            return Ok();
         }
     }
 }
--- MKT_POLOSYS_API/Providers/PoloDukcapilProvider.cs
+++ MKT_POLOSYS_API/Providers/PoloDukcapilProvider.cs
@@ -20,10 +20,11 @@ namespace MKT_POLOSYS_API.Providers
         public static async Task getDukcapilQueue(string sourceData, string queueUID)
         {
             var connectionString = context.Database.GetDbConnection().ConnectionString;
-            //List<ProcessResultModel> mMktPoloQuestionLabels = new List<ProcessResultModel>();
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                // Declare Cnnection                
+                #region prepare connection for fetching dukcapil queue
+                // Declare Connection                
                 string sproc = "spMKT_POLO_GETDUKCAPILQUEUE";
                 SqlCommand command = new SqlCommand(sproc, connection);
                 command.CommandType = CommandType.StoredProcedure;
@@ -34,29 +35,48 @@ namespace MKT_POLOSYS_API.Providers
 
                 // Open Connection
                 command.Connection.Open();
+                #endregion
 
+                #region fetch record and store to a list of dictionary
                 List<Dictionary<string, object>> queue = new List<Dictionary<string, object>>();
                 SqlDataReader rd = command.ExecuteReader();
-                while(rd.Read())
+                while (rd.Read())
                 {
                     queue.Add(Enumerable.Range(0, rd.FieldCount).ToDictionary(rd.GetName, rd.GetValue));
                 }
+                #endregion
 
+                #region closing connection for later usage
+                rd.Close();
+                command.Connection.Close();
+                #endregion
+
+                #region consume womf dukcapil API for each record
                 foreach (Dictionary<string, object> q in queue)
                 {
+                    //consuming womf dukcapil API
                     var result = await consumeDukcapil(q);
 
+                    //prepare connection for update dukcapil result
                     sproc = "spMKT_POLO_UPDATEDUKCAPILRESULT";
                     command = new SqlCommand(sproc, connection);
                     command.CommandType = CommandType.StoredProcedure;
 
-                    // Define Query Parameter
-                    command.Parameters.AddWithValue("@sourceData", sourceData);
-                    command.Parameters.AddWithValue("@queueUID", queueUID);
-
-                    // Open Connection
                     command.Connection.Open();
+
+                    //define query parameter
+                    command.Parameters.AddWithValue("@T_MKT_POLO_DUKCAPIL_CHECK_QUEUE_ID", q["T_MKT_POLO_DUKCAPIL_CHECK_QUEUE_ID"]);
+                    command.Parameters.AddWithValue("@responseCode", result["ResponseCode"]);
+                    command.Parameters.AddWithValue("@responseMessage", result["ResponseMessage"]);
+                    command.Parameters.AddWithValue("@statusDukcapil", result["FinalResult"]);
+
+                    rd = command.ExecuteReader();
+
+                    //close connection
+                    command.Connection.Close();
+                    rd.Close();
                 }
+                #endregion
 
                 //Connection Close
                 command.Connection.Close();
@@ -65,7 +85,7 @@ namespace MKT_POLOSYS_API.Providers
 
         private static async Task<Dictionary<string, object>> consumeDukcapil(Dictionary<string, object> data)
         {
-            #region ignore cert validation
+            #region ignore ssl cert validation
             var clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback += (sender, certificate, chain, errors) =>
             {
@@ -90,6 +110,7 @@ namespace MKT_POLOSYS_API.Providers
             body.Add("SOURCE", "MarketingPool");
             #endregion
 
+            #region consume womf dukcapil API and return response
             HttpClient client = new HttpClient(clientHandler);
             client.BaseAddress = new Uri("https://10.0.9.227/api/dukcapil/");
 
@@ -102,6 +123,7 @@ namespace MKT_POLOSYS_API.Providers
                 var result = JsonConvert.DeserializeObject<Dictionary<string, object>>(apiResponse);
                 return result;
             }
+            #endregion
         }
     }
 }
