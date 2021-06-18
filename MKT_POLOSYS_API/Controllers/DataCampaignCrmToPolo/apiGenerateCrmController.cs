ad05e6b3949d4e290b3463d4e941dbfdf821f0e5
maulana06 <bron.maulana@gmail.com>
2021-06-18 08:43:10 +0700

    contoh response array

---
+++ MKT_POLOSYS_API/Controllers/DataCampaignCrmToPolo/apiGenerateCrmController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MKT_POLOSYS_API.Models;
+using MKT_POLOSYS_API.Providers.DataCampaignCrmToPolo;
+
+ 
+namespace MKT_POLOSYS_API.Controllers.DataCampaignCrmToPolo
+{
+    [Route("api/apiGenerateCrm")]
+    [ApiController]
+    public class apiGenerateCrmController : ControllerBase
+    {
+        // GET: api/<apiUploadStatusMssWiseController>
+        
+        [Produces("application/json")]
+
+        [HttpPost("procGenerateDataCrm")]
+        public async Task<IActionResult> postGenerateDataCrm()
+        {
+            
+            string parameterBody = "";
+            using (StreamReader stream = new StreamReader(Request.Body))
+            {
+                parameterBody = await stream.ReadToEndAsync();
+            }
+            apiGenerateCrmProvider procGenDataCrm = new apiGenerateCrmProvider();
+            var data = procGenDataCrm.procGenDataCrm(parameterBody);
+
+            if (data.Count == 0)
+            {
+                return NotFound(data);
+            }
+            return Ok(data);
+        }
+
+
+
+    }
+}
---
+++ MKT_POLOSYS_API/Models/ProcessResultDetailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MKT_POLOSYS_API.Models
+{
+    public class ProcessResultDetailModel
+    {
+        public string responseCode { get; set; }
+        public string responseMessage { get; set; }
+        public string errorMessage { get; set; }
+        public List<ErrorDetailModel> ListDetail { get; set; }
+    }
+}
