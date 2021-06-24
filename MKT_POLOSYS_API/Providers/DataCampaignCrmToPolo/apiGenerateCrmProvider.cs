cace23f7146e44c236b68f8cec8787ccdb0fe0a7
maulana06 <bron.maulana@gmail.com>
2021-06-24 12:42:29 +0700

    error

--- MKT_POLOSYS_API/Providers/DataCampaignCrmToPolo/apiGenerateCrmProvider.cs
+++ MKT_POLOSYS_API/Providers/DataCampaignCrmToPolo/apiGenerateCrmProvider.cs
@@ -10,53 +10,65 @@ using System.Threading.Tasks;
 namespace MKT_POLOSYS_API.Providers.DataCampaignCrmToPolo
 {
     public class apiGenerateCrmProvider
-    {   
+    {
         private static WISE_STAGINGContext context = new WISE_STAGINGContext();
         private static string connectionString = context.Database.GetDbConnection().ConnectionString;
         SqlConnection connection = new SqlConnection(connectionString);
         public List<ProcessResultDetailModel> procGenDataCrm(string parameterBody, out string done, string guid)
         {
-            var connectionString = context.Database.GetDbConnection().ConnectionString;
             List<ProcessResultDetailModel> procGenDataCrm = new List<ProcessResultDetailModel>();
-            List<ErrorDetailModel> ListDetailError = new List<ErrorDetailModel>();
-            //Declare COnnection                
-            var querySstring = "spMKT_POLOCRM_APIGENERATECAMPAIGN_VAL";
-            SqlCommand command = new SqlCommand(querySstring, connection);
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-
-            //Define Query Parameter
-            command.Parameters.AddWithValue("@parameterBody", parameterBody);
-            command.Parameters.AddWithValue("@guid", guid);
-
-            //open Connection
-            command.Connection.Open();
-
-            //PRoses Sp
-            SqlDataReader rd = command.ExecuteReader();
-            ProcessResultDetailModel data = new ProcessResultDetailModel();
-            while (rd.Read())
+            try
             {
-                data.responseCode = rd[0].ToString();
-                data.responseMessage = rd[1].ToString();
-                //data.errorMessage = rd[2].ToString();
-            }
-            if (rd.NextResult())
-            {
+                var connectionString = context.Database.GetDbConnection().ConnectionString;
+                List<ErrorDetailModel> ListDetailError = new List<ErrorDetailModel>();
+                //Declare COnnection                
+                var querySstring = "spMKT_POLOCRM_APIGENERATECAMPAIGN_VAL";
+                SqlCommand command = new SqlCommand(querySstring, connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                //Define Query Parameter
+                command.Parameters.AddWithValue("@parameterBody", parameterBody);
+                command.Parameters.AddWithValue("@guid", guid);
+
+                //open Connection
+                command.Connection.Open();
+
+                //PRoses Sp
+                SqlDataReader rd = command.ExecuteReader();
+                ProcessResultDetailModel data = new ProcessResultDetailModel();
                 while (rd.Read())
                 {
-                    ErrorDetailModel listError = new ErrorDetailModel();
-                    listError.taskId = rd[0].ToString();
-                    listError.errDesc = rd[1].ToString();
-                    ListDetailError.Add(listError);
+                    data.responseCode = rd[0].ToString();
+                    data.responseMessage = rd[1].ToString();
+                    //data.errorMessage = rd[2].ToString();
                 }
-                data.errorMessage = ListDetailError.ToList();
+                if (rd.NextResult())
+                {
+                    while (rd.Read())
+                    {
+                        ErrorDetailModel listError = new ErrorDetailModel();
+                        listError.taskId = rd[0].ToString();
+                        listError.errDesc = rd[1].ToString();
+                        ListDetailError.Add(listError);
+                    }
+                    data.errorMessage = ListDetailError.ToList();
+                }
+                procGenDataCrm.Add(data);
+
+                //Connection Close
+                command.Connection.Close();
+
+                done = "done";
             }
-            procGenDataCrm.Add(data);
-
-            //Connection Close
-            command.Connection.Close();
-
-            done = "done";
+            catch (Exception e)
+            {
+                Console.WriteLine("Execute StoreProcedure Validation Error..");
+                ProcessResultDetailModel data = new ProcessResultDetailModel();
+                data.responseCode = "02";
+                data.responseMessage = e.Message;
+                done = "not done";
+                procGenDataCrm.Add(data);
+            }
             if (done == "done")
             {
                 Task.Run(() => UpdateCrmFlag(parameterBody, guid));
